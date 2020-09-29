4d59631cf93bcdc66cab8645615ad0d558da6122
Chris Cole <cccole3@gmail.com>
2020-09-28 19:44:11 -0500

    adding material ui + creat react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Nav } from "./components/Nav";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <h1>g</h1>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
