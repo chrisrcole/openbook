b5bb191d3623e30e3bab43cafd32e1e66ccef78e
Chris Cole <cccole3@gmail.com>
2020-09-28 22:26:22 -0500

    Updates

---
+++ precache-manifest.8cb88f935520a9511aa3b0f8424a101c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b55d4e610cc33bb935704611f2c2bd",
+    "url": "/openbook/index.html"
+  },
+  {
+    "revision": "febb486e731015e5af95",
+    "url": "/openbook/static/js/2.a6943dbb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/openbook/static/js/2.a6943dbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67960d5f883e9134413b",
+    "url": "/openbook/static/js/main.f0ed85c6.chunk.js"
+  },
+  {
+    "revision": "76084ccd1ac999da4645",
+    "url": "/openbook/static/js/runtime-main.93c7d5bb.js"
+  },
+  {
+    "revision": "da4382b12dfab88c1a367688fb047ad4",
+    "url": "/openbook/static/media/food.da4382b1.jpg"
+  },
+  {
+    "revision": "87b537e75bffeb9b6090baf1a54229fe",
+    "url": "/openbook/static/media/portrait.87b537e7.jpg"
+  }
+]);
